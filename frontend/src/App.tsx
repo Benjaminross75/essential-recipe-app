d2cacab1f63c004cb8cb9a70e306f51295df0649
Ben Ross <mrbenross@gmail.com>
2024-03-05 11:23:10 -0600

    recipe state established

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
+import './App.css';
+import {useState, useEffect, FormEvent, useRef} from 'react';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("burgers");
+  const [recipes, setRecipes] = useState([])
+
 return(
   <div>Hello from Recipe App</div>
 )

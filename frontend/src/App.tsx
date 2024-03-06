ada2cd3e572cac8c3bf5cc155f53a4f2fc5e1e5e
Ben Ross <mrbenross@gmail.com>
2024-03-06 08:34:32 -0600

    handleSearchSubmit function created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 import {useState, useEffect, FormEvent, useRef} from 'react';
-
+import * as api from './api';
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("burgers");
   const [recipes, setRecipes] = useState([])
 
+  const handleSearchSubmit = async () => {
+    try {
+     const recipes = await api.searchRecipes(searchTerm, 1);
+     setRecipes(recipes);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 return(
   <div>Hello from Recipe App</div>
 )

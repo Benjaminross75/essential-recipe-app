85652983a6af67b24cb643d16321c9b4af1086bf
Ben Ross <mrbenross@gmail.com>
2024-03-06 08:47:37 -0600

    api.ts file created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
 import './App.css';
 import {useState, useEffect, FormEvent, useRef} from 'react';
 import * as api from './api';
+import { Recipe } from './types';
+
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("burgers");
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
 
   const handleSearchSubmit = async () => {
     try {
      const recipes = await api.searchRecipes(searchTerm, 1);
      setRecipes(recipes);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 return(
-  <div>Hello from Recipe App</div>
-)
-}
+  <div>
+    <form onSubmit={()=> handleSearchSubmit()}>
+      <button type='submit'>Submit</button>
+    </form>
+    {recipes.map((recipe)=> (
+      <div>
+         recipe image location: {recipe.image}
+         recipe title: {recipe.title}
+      </div>
+    ))}
+  </div>
+);
+};
 
 
 export default App

11ff3ecb51dc16544a6520e676f317ad097348fa
Ben Ross <mrbenross@gmail.com>
2024-03-06 09:16:25 -0600

    form added to App.jsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,27 @@ import { Recipe } from './types';
 
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState<string>("burgers");
+  const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-  const handleSearchSubmit = async () => {
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
     try {
      const recipes = await api.searchRecipes(searchTerm, 1);
-     setRecipes(recipes);
+     setRecipes(recipes.results);
     } catch (error) {
       console.log(error);
     }
   }
 return(
   <div>
-    <form onSubmit={()=> handleSearchSubmit()}>
+    <form onSubmit={(event)=> handleSearchSubmit(event)}>
+      <input type='text'
+             required
+             placeholder='Search for a Recipe...'
+             value={searchTerm}
+             onChange={(event)=> setSearchTerm(event.target.value)}
+      ></input>
       <button type='submit'>Submit</button>
     </form>
     {recipes.map((recipe)=> (
